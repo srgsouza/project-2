f024370b7bff6bfd91cf906c4aad21da252c8350
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-25 13:31:04 -0600

    mtbikeprjct api working, started search page for data, some styling done

--- controllers/trails.js
+++ controllers/trails.js
@@ -5,16 +5,13 @@ const request = require('request');
 
 router.get('/search', (req, res) => {
 	let mountainBikeProject = "200320520-bb520cea5200b21d7530c95bf2166f64";
-	request("https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200320520-bb520cea5200b21d7530c95bf2166f64", (err, response, body) => {
+	request("https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&maxResults=6&key=200320520-bb520cea5200b21d7530c95bf2166f64", (err, response, body) => {
 		console.log(err);
-		// console.log(response);
-		console.log(body);
+		body = JSON.parse(body)
 		res.render ('trails/search.ejs',
 		{body: body});
 	});
 });
-
-
 
 router.get('/', async (req, res) => {
 	try {
